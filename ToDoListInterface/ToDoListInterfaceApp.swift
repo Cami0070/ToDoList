94012ef93d26e4732947492b62f198abb2c29575
CamiBraghes <cameliabraghes@gmail.com>
2022-01-18 19:01:16 +0200

    Initial Commit

---
+++ ToDoListInterface/ToDoListInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListInterfaceApp.swift
+//  ToDoListInterface
+//
+//  Created by Camelia Braghes on 18.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
